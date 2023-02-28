dc8b727d10880214c45e18884305c69a12097d25
username <email@blahblah.com>
2023-02-28 13:07:05 +0200

    Initial package commit

---
+++ Sources/iOS-Network-Layer/iOS_Network_Layer.swift
@@ -0,0 +1,6 @@
+public struct iOS_Network_Layer {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/iOS-Network-LayerTests/iOS_Network_LayerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import iOS_Network_Layer
+
+final class iOS_Network_LayerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(iOS_Network_Layer().text, "Hello, World!")
+    }
+}
